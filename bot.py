a478f8a50ff946f36abaa79f9b9f15258ac9337d
Darzhain Dmitrii <dimad_1996@mail.ru>
2019-03-21 16:42:57 +0700

    edited bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,5 @@
 import telebot
-import requests
 import config
-
-from telebot import apihelper
-
-# proxies = {
-#   'http': 'http://10.10.1.10:3128',
-#   #'https': 'https://222.165.249.43:23500',
-#   'https': 'https://45.125.61.209:32804',
-# }
-
-#token = '731742271:AAEZy417zuGC8jOOLLts7JBBqx_8Pio-mBg'
-#url = 'https://api.telegram.org/bot731742271:AAEZy417zuGC8jOOLLts7JBBqx_8Pio-mBg'
-#s = requests.session()
-#s.proxies = proxies
-#r = s.get('https://api.telegram.org/bot731742271:AAEZy417zuGC8jOOLLts7JBBqx_8Pio-mBg/getUpdates')
-#apihelper.proxy = {'https':'socks5://login:pass@12.11.22.33:8000'}
-#print(r.json())
 
 bot = telebot.TeleBot(config.token)
 
